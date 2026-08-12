6e04ed6e065f7c8a43c9b0694c678a5b5f8355a2
Dakshit Makwana <69599534+dakshit02q@users.noreply.github.com>
2026-08-13 00:57:39 +0530

    Solve la-matrix-trace

---
+++ study-plans/math-linear-algebra/la-matrix-trace/la-matrix-trace.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def matrix_trace(A):
+    """
+    Returns: float, the trace (sum of diagonal elements) of A.
+    """
+    A = np.asarray(A)
+    trace = 0
+    for i in range(min(A.shape)):
+        trace += A[i][i]
+
+    return trace
